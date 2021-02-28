867907c81e15b5a7daade499d0a2a17fb9165bfe
beomjo <qjatjr1108@gmail.com>
2021-02-28 18:37:52 +0900

    style: Change package name

--- src/androidTest/java/com/beomjo/compilation/ExampleInstrumentedTest.kt
+++ src/androidTest/java/com/beomjo/compilation/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.beomjo.android.compilation
+package com.beomjo.compilation
 
 import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnit4
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.beomjo.android.compilation.test", appContext.packageName)
+        assertEquals("com.beomjo.compilation.test", appContext.packageName)
     }
 }
