6d7d77f25256f28b373686912defb9a3f83c660f
beomjo <qjatjr1108@gmail.com>
2021-03-07 17:47:23 +0900

    test: VolatieLiveData mockk로 변경

--- src/test/java/com/beomjo/compilation/util/VolatileLiveDataTest.kt
+++ src/test/java/com/beomjo/compilation/util/VolatileLiveDataTest.kt
@@ -3,22 +3,31 @@ package com.beomjo.compilation.util
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
+import io.mockk.*
 import org.junit.Assert
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
 class VolatileLiveDataTest {
 
+    @Before
+    fun setUp() {
+        MockKAnnotations.init(this)
+    }
+
     @Rule
     @JvmField
-    val instantTaskExecutorRule = InstantTaskExecutorRule() // Force tests to be executed synchronously
+    val instantTaskExecutorRule =
+        InstantTaskExecutorRule() // Force tests to be executed synchronously
 
     @Test
     fun `MutableLiveData Observe 해제후 다시 Observe 햇을떄, 기존에 방출햇던 데이터 재방출`() {
         //Given
-        val result = mutableListOf<String>()
         val liveData = MutableLiveData<String>()
-        val observer = Observer<String> { result.add(it) }
+        val observer = mockk<Observer<String>> {
+            every { onChanged("1") } just Runs
+        }
         liveData.observeForever(observer)
 
         //When
@@ -27,16 +36,19 @@ class VolatileLiveDataTest {
         liveData.observeForever(observer)
 
         //Then
-        Assert.assertEquals(listOf("1", "1"), result)
-        liveData.removeObserver(observer)
+        verify(exactly = 2) {
+            observer.onChanged(eq("1"))
+            observer.onChanged(eq("1"))
+        }
     }
 
     @Test
     fun `VolatileLiveData Observe 해제후 다시 Observe 햇을떄, 기존에 방출했던 데이터 다시 방출하지않음`() {
         //Given
-        val result = mutableListOf<String>()
         val liveData = VolatileLiveData<String>()
-        val observer = Observer<String> { result.add(it) }
+        val observer = mockk<Observer<String>> {
+            every { onChanged("1") } just Runs
+        }
         liveData.observeForever(observer)
 
         //When
@@ -45,7 +57,6 @@ class VolatileLiveDataTest {
         liveData.observeForever(observer)
 
         //Then
-        Assert.assertEquals(listOf("1"), result)
-        liveData.removeObserver(observer)
+        verify(exactly = 1) { observer.onChanged(eq("1")) }
     }
 }
